26e72896e5b007f92aed396fdefc842e62ebd6d0
ronjr <ron@coheresoftware.com>
2017-01-22 09:53:06 -0600

    Create settings.inc.php

---
+++ VandyGit/settings.inc.php
@@ -0,0 +1,13 @@
+<?php 
+
+date_default_timezone_set('America/Chicago');
+define('FILEROOT', '/pathtorootfolder/VandyGit/'); // This should be the folder that contains the BO and BLL folders
+define('SITEROOT', 'http://mywebsite.com/'); // This should be your website name
+define('SITENAME', 'Vandy Git Project');
+define('DBHOST','localhost'); // hostname for your database server
+define('DBPORT','3306'); 
+define('DBDATABASE','vandygit'); // database name
+define('DBUSER','db_user'); // database user
+define('DBPASS','db_pass'); // database password
+
+?>
